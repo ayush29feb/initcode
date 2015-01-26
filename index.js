6fda8576b28360a38bfff20a34c5761aaba749e9
Ayush <ayush29f@uw.edu>
2015-01-26 05:44:47 -0800

    uninstalled opimistic

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ var fs = require('fs'),
 	program = require('commander'),
 	repl = require('repl');
 
+program
+    .version('0.0.1')
+    .usage('<keywords>')
+    .parse(process.argv);
 
-var json = {
-	"lang": argv.lang
-};
+console.log(program.args);
 
-fs.writeFile("schema.json", JSON.stringify(json, null, 4), function(err){
-	if(err){
-		console.log(err);
-	} else {
-		console.log("Awesome! It works.");
-	}
-});
+// var json = {
+// 	"lang": argv.lang
+// };
+
+// fs.writeFile("schema.json", JSON.stringify(json, null, 4), function(err){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log("Awesome! It works.");
+// 	}
+// });
