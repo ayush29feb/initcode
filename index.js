a4775d032a5632daa91630ac0c4d073a021387d6
Ayush <ayush29f@uw.edu>
2015-01-26 16:41:42 -0800

    Basic Reading Funcitonality Done

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ if(program.args.length != 3) {
 }
 
 
-// ReadingFile
-// fs.readFile(program.args[0], 'utf8', function (err, data) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log(data);
-// });
-
 // Command Line Input
 // var rl = readline.createInterface({
 //   input: process.stdin,
--- writers/java.js
+++ writers/java.js
@@ -1,12 +1,63 @@
 var fs = require('fs'),
 	str = require('string');
 
+var readClass = function(data, files, superClass) {
+	var code = {
+		className: null,
+		isMain: false,
+		superClass: null,
+		fields: [],
+		methods: []
+	}
+	if(superClass){
+		code.superClass = superClass;
+	}
+	// Do Stuff to fill the code
+	var lines = str(data).lines();
+	for(var i = 0; i < lines.length; i++) {
+		var curLine = str(lines[i] + ' ');
+		// Push The Class Name
+		// later add the option funcitonality
+		if(!curLine.startsWith('\t')){
+			code.className = curLine.substr(0, curLine.indexOf(' ')).s;
+			code.isMain = curLine.contains(' -m ');
+		} else {
+			curLine = curLine.substr(1);
+			if(curLine.startsWith('--')) {
+				curLine = curLine.substr(1);
+				var method = {
+					name: curLine.substr(0, curLine.indexOf(' ')).s
+				}
+				code.methods.push(method);
+			} else if (curLine.startsWith('-')) {
+				curLine = curLine.substr(2);
+				var field = {
+					name: curLine.substr(0, curLine.indexOf(' ')).s
+				}
+				code.fields.push(field);
+			} else {
+				readClass(curLine.s, files, code.className);
+			}
+		}
+	}
+
+	// Push it to the files
+	files.push(code);
+}
+
+var writeClass = function(codeObject, dir) {
+	// Create Buffer
+
+
+	// Create the file
+}
+
 module.exports = {
 	data: null,
 	files: [],
 	read: function(data) {
-		var lines = str(data).lines();
-		console.log(lines);
+		readClass(data, this.files);
+		console.log(this.files);
 	},
 	write: function(dir) {
 
